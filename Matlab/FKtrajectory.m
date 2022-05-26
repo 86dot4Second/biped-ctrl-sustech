3b5ce89eb3fc4dd1c427609d273056dc58bc2aef
皓天 崔 <360791883@qq.com>
2022-05-26 14:31:05 +0800

    add code of dynamic

--- Matlab/FKtrajectory.m
+++ Matlab/FKtrajectory.m
@@ -1,4 +1,4 @@
-clear;
+% clear;
 clc;
 pos = zeros(3,51);
 forces = zeros(5,51);
@@ -33,25 +33,36 @@ for i=0:50
     % 计算
     testFD;
     
-    pos(:,i+1) = pm_j6(1:3,4);
+    pos(:,i+1) = ee(1:3,4);
     forces(:,i+1) = abs(actuation_force);
     %forces(:,i+1) = n1(4:5);
-    %vel(:,i+1) = v6(4:6);
-    vel(:,i+1) = v6(1:3)+cross(v6(4:6),pm_j6(1:3,4));
+    vel(:,i+1) = a3(4:6);
+    %vel(:,i+1) = v9(1:3)+cross(v9(4:6),pm_j10(1:3,4));
+    %vel(:,i+1) = a3(1:3)+cross(a3(4:6),pm_j3(1:3,4))+cross(v3(4:6),v3(1:3))+C3(v3(4:6))*C3(v3(4:6))*pm_j3(1:3,4);
 end
 
 t = 0:0.1:5;
+% plot(t,j2pos(:,1),'LineWidth',1.2)
+% hold on;
+% plot(t,j2pos(:,2),'LineWidth',1.2)
+% hold on;
+% plot(t,j2pos(:,3),'LineWidth',1.2)
+% hold on
 % plot(t,pos(1,:))
 % hold on;
 % plot(t,pos(2,:))
 % hold on;
 % plot(t,pos(3,:))
-plot(t,vel(1,:))
-hold on;
-plot(t,vel(2,:))
-hold on;
-plot(t,vel(3,:))
+% axis([0 5 -1 1])
+% plot(t,vel(1,:))
+% hold on;
+% plot(t,vel(2,:))
+% hold on;
+% plot(t,vel(3,:))
 % legend('x','y','z');
+plot(t,acf,'k','LineWidth',1.2)
+hold on
+plot(t,forces(5,:),'--r','LineWidth',2),title('torque5')
 % plot(t,forces(1,:))
 % figure
 % plot(t,forces(2,:))
@@ -61,3 +72,10 @@ plot(t,vel(3,:))
 % plot(t,forces(4,:))
 % figure
 % plot(t,forces(5,:))
+
+% subplot(3,1,1),plot(t,lal(:,1),'k','LineWidth',1.2),hold on
+% plot(t,vel(1,:),'--r','LineWidth',2),title('L3\_alphax')
+% subplot(3,1,2),plot(t,lal(:,2),'k','LineWidth',1.2),hold on
+% plot(t,vel(2,:),'--r','LineWidth',2),title('L3\_alphay')
+% subplot(3,1,3),plot(t,lal(:,3),'k','LineWidth',1.2),hold on
+% plot(t,vel(3,:),'--r','LineWidth',2),title('L3\_alphaz')
